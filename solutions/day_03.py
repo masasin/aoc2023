14eeb149675f60d88ecb6c25207cf6981c08e30a
jnassar <jn.masasin@gmail.com>
2023-12-04 16:24:45 +0100

    Make adjacent number coords more generic

--- solutions/day_03.py
+++ solutions/day_03.py
@@ -1,4 +1,3 @@
-# %%
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,14 +7,19 @@ from scipy.ndimage import convolve
 import numpy as np
 
 
-def adjacent_number_coords(grid):
-    grid = np.pad(grid, pad_width=1, mode="constant", constant_values=".")
-    num_mask = np.char.isdigit(grid)
-    sym_mask = (grid != ".") & ~num_mask
+def sym_mask(grid):
+    return (grid != ".") & ~np.char.isdigit(grid)
+
+
+def gear_mask(grid):
+    return grid == "*"
+
+
+def adjacent_number_coords(mask):
     kernel = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
-    convolved_sym_mask = convolve(sym_mask.astype(int), kernel, mode="constant", cval=0)
-    adjacent_number_mask = (convolved_sym_mask > 0) & num_mask
-    return np.argwhere(adjacent_number_mask) - np.array([1, 1])
+    convolved_mask = convolve(mask.astype(int), kernel, mode="constant", cval=0)
+    adjacent_number_mask = (convolved_mask > 0) & num_mask
+    return np.argwhere(adjacent_number_mask)
 
 
 def drop_contiguous_points(coords):
@@ -30,9 +34,8 @@ def drop_contiguous_points(coords):
     return coords[mask]
 
 
-def extract_contiguous_number(row, col, grid):
+def extract_contiguous_number(row, col):
     number = ""
-    num_mask = np.char.isdigit(grid)
     i = col
     while i >= 0 and num_mask[row, i]:
         number = grid[row, i] + number
@@ -46,19 +49,23 @@ def extract_contiguous_number(row, col, grid):
     return int(number)
 
 
-def extract_numbers(coords, grid):
+def extract_numbers(coords):
     for row, col in coords:
-        yield extract_contiguous_number(row, col, grid)
+        yield extract_contiguous_number(row, col)
 
 
-def solve_p1(grid):
-    unique_contiguous_numbers = extract_numbers(
-        drop_contiguous_points(adjacent_number_coords(grid)), grid
-    )
+def solve_p1():
+    mask = sym_mask(grid)
+    adjacent_coords = drop_contiguous_points(adjacent_number_coords(mask))
+    unique_contiguous_numbers = extract_numbers(adjacent_coords)
     return sum(unique_contiguous_numbers)
 
 
 if __name__ == "__main__":
     grid = np.array(list(map(list, data.splitlines())))
-    answer_p1 = solve_p1(grid)
+    grid = np.pad(grid, pad_width=1, mode="constant", constant_values=".")
+    num_mask = np.char.isdigit(grid)
+    answer_p1 = solve_p1()
     print(answer_p1)
+
+# %%
