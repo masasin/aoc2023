b2cab84a516d545d838f1cfaa8df1fd4eae83f76
jnassar <jn.masasin@gmail.com>
2023-12-04 17:17:57 +0100

    Add Day 4 Part 2

--- solutions/day_04.py
+++ solutions/day_04.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,12 +12,33 @@ def parse_line(line):
     return set(map(int, winning.split())), set(map(int, yours.split()))
 
 
+def parse(data):
+    yield from (parse_line(line) for line in data.splitlines())
+
+
+def n_matches(winning, yours):
+    return len(winning & yours)
+
+
 def score_p1(winning, yours):
-    return int(2 ** (len(winning & yours) - 1))
+    return int(2 ** (n_matches(winning, yours) - 1))
 
 
 def solve_p1(data):
-    return sum(score_p1(*parse_line(line)) for line in data.splitlines())
+    return sum(score_p1(*line) for line in parse(data))
 
 
-submit(solve_p1(data))
+def solve_p2(data):
+    copies = defaultdict(lambda: 1)
+
+    for i, (winning, yours) in enumerate(parse(data)):
+        for j in range(i + 1, i + 1 + n_matches(winning, yours)):
+            copies[j] += copies[i]
+        copies[i]  # ensure current card is counted
+
+    return sum(copies.values())
+
+
+if __name__ == "__main__":
+    submit(solve_p1(data), part=1)
+    submit(solve_p2(data), part=2)
